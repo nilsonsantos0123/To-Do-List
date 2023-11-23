4e035d969099914fe6a5443b10c21a653252e9f7
nilsonsantos0123 <github.com/nilsonsantos0123>
2023-11-23 10:18:54 -0300

    primeiro commit

---
+++ scripts.js
@@ -0,0 +1,61 @@
+const input = document.querySelector(".input-task");
+const button = document.querySelector(".button-add-task");
+const listaCompleta = document.querySelector(".list-tasks");
+
+let minhaListaDeItens = [];
+
+function adicionarNovaTarefa() {
+  minhaListaDeItens.push({
+    tarefa: input.value,
+    cocluida: false,
+  });
+
+  input.value = "";
+
+  mostrarTarefas();
+}
+
+function mostrarTarefas() {
+  let novaLi = "";
+
+  minhaListaDeItens.forEach((item, posicao) => {
+    novaLi =
+      novaLi +
+      `
+
+    <li class="task ${item.cocluida && "done"}">
+         <img src="imagem/checked.png" alt="check-na-tarefa" onclick = 'concluirTarefa(${posicao})' />
+        <p>${item.tarefa}</p>
+        <img src="imagem/trash.png" alt="tarefa-lixeira" onclick='deletarItem(${posicao})'/>
+    </li>`;
+  });
+
+  listaCompleta.innerHTML = novaLi;
+
+  localStorage.setItem("lista", JSON.stringify(minhaListaDeItens));
+}
+
+function concluirTarefa(posicao) {
+  minhaListaDeItens[posicao].cocluida = !minhaListaDeItens[posicao].cocluida;
+
+  mostrarTarefas();
+}
+
+function deletarItem(posicao) {
+  minhaListaDeItens.splice(posicao, 1);
+
+  mostrarTarefas();
+}
+
+function recarregarTarefas() {
+  const tarefasDoLocalStorage = localStorage.getItem("lista");
+
+  if (tarefasDoLocalStorage) {
+    minhaListaDeItens = JSON.parse(tarefasDoLocalStorage);
+  }
+
+  mostrarTarefas();
+}
+
+recarregarTarefas();
+button.addEventListener("click", adicionarNovaTarefa);
